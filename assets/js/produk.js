673e1ba03ad590853f106c3332a15615ee39ca2c
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-10-20 15:45:18 +0700

    create js produk

--- assets/js/produk.js
+++ assets/js/produk.js
@@ -1,15 +1,15 @@
-const network = "https://6530b4526c756603295ef07e.mockapi.io/";
+// const network = "https://6530b4526c756603295ef07e.mockapi.io/";
+const url = new URL ('https://6530b4526c756603295ef07e.mockapi.io/Produk');
 
 const listProduct = document.getElementById("list-product");
-async function fetchListProduct(){
-    const res = await fetch(
-        "https://6530b4526c756603295ef07e.mockapi.io/Produk"
-    );
+async function getDataProduct() {
+  try {
+    let respons = await fetch(url)
+    let product = await respons.json()
 
-    const data = res.json();
-    data.map((item) => {
-        listProduct.innerHTML += `
-        <div class="card bg-dark mx-1" style="width: 18rem; height: 20rem;">
+    product.map((item) => {
+      listProduct.innerHTML += `
+        <div class="card bg-dark mx-1 mb-5" style="width: 18rem; height: 20rem;">
                 <img class="card-img-top img-product" src="${item.avatar}" alt="Card image cap">
                 <div class="card-body">
                   <h6 class="card-text">${item.name}</h6>
@@ -18,5 +18,56 @@ async function fetchListProduct(){
                 </div>
               </div>
         `;
-    });
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+getDataProduct();
+
+//Filter Plastik
+const plastik = document.getElementById("plastik")
+plastik.onclick = function () { 
+
+  getProductFilter("Plastik");
+}
+
+//Filter Kertas
+const kertas = document.getElementById("kertas")
+kertas.onclick = function () { 
+
+  getProductFilter("Kertas");
+}
+
+//Filter Kardus
+const kardus = document.getElementById("kardus")
+kardus.onclick = function () { 
+
+  getProductFilter("Kardus");
+}
+
+async function getProductFilter(filterParam) {
+  listProduct.innerHTML = ``;
+  try {
+    url.searchParams.delete('search')
+    url.searchParams.append('search', filterParam);
+    let respons = await fetch(url)
+    let product = await respons.json()
+
+    product.map((items) => {
+      listProduct.innerHTML += `
+      <div class="card bg-dark mx-1 mb-5" style="width: 18rem; height: 20rem;">
+              <img class="card-img-top img-product" src="${items.avatar}" alt="Card image cap">
+              <div class="card-body">
+                <h6 class="card-text">${items.name}</h6>
+                <p class="opacity-75">${items.Deskripsi}</p>
+                <p>${items.Harga}</p>
+              </div>
+            </div>
+      `;
+    })
+  } catch (error) {
+    console.log(error)
+  }
+};
